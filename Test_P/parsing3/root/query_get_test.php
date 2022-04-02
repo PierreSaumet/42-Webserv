6c3d3f778ab98e889d4e998ceecef262fb22a0c6
user42 <psaumet@student.42.fr>
2022-04-02 15:00:05 +0200

    changing page2, now go pack to index. and query_get to try display result

--- Test_P/parsing3/root/query_get_test.php
+++ Test_P/parsing3/root/query_get_test.php
@@ -1,14 +1,18 @@
 <html>
  <head>
   <title>GET Query String</title>
-  <link rel="stylesheet" type="text/css" href="/static/style1.css">
+  <style>body { background-color: rgb(105, 140, 238) }
+			h1 { font-size:2cm; text-align: center; color: rgb(121, 219, 56);
+			text-shadow: 0 0 2mm red}
+		</style>
  </head>
- <body>
+
+
  <body>
     <h1> Im'gonne say hello to you using the query string in the URL ! </h1>
    <h2>
  <?php
-    echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+    echo 'Hello ' . htmlspecialchars($_GET["user_name"]) . '!';
 ?>
     </h2>
  </body>
